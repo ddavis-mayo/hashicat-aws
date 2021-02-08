f7001402e7d39724fd0eb5a2bb6932d4b5478e9a
Dylan <davis.dylan@mayo.edu>
2021-02-08 18:24:17 +0000

    added s3 bucket mod

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,10 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/DylanDavis-training/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
-    bucket_prefix = "prefix"
-    module "s3-bucket" {
-  source  = "app.terraform.io/DylanDavis-training/s3-bucket/aws"
-  version = "1.15.0"
-  # insert required variables her
-}
+    bucket_prefix = "{$prefix}"
+   
 }
